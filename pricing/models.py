9fe5d85522b1a559c7acea479f1e5c7405799510
Rafael Estrada <ntaticat@gmail.com>
2023-01-04 22:24:42 -0600

    add viewmodels

--- pricing/models.py
+++ pricing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Pricing(models.Model):
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+
+class ProductPricing(models.Model):
+    pricing = models.ForeignKey(Pricing, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
--- pricing/views.py
+++ pricing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hola Mundo</h1>")
+
+def about(request):
+    return HttpResponse("<p>Hello, Its me, can you hear me?</p>")
